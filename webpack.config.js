772f1ca46e3cad054cc895bf62ba3af7fd26795e
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-06-25 20:36:45 +0530

    Initial Project setup
    Setup webpack with basic configuration. Add npm scripts for development.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject: "head",
+            scriptLoading: "defer"
+        })
+    ]
+}
