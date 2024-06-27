4b8493c66b48e900dc4e5e30bc42652102d11328
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-06-27 20:05:21 +0530

    Add responsiveness

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+const menuCloseBtn = document.querySelector(".menu-close-btn");
+const menuOpenBtn = document.querySelector(".menu-open-btn");
+const nav = document.querySelector("nav");
+const toggleMenu = () => {
+    if (nav.style.width === "0px") {
+        nav.style.width = "300px";
+        menuOpenBtn.style.display = "none";
+    }
+    else {
+        nav.style.width = "0px";
+        menuOpenBtn.style.display = "block";
+        nav.style.overflow = "hidden";
+    }
+}
+
+const setupNavEvents = () => {
+    menuCloseBtn.addEventListener("click", toggleMenu);
+    menuOpenBtn.addEventListener("click", toggleMenu);
+}
+
+export default setupNavEvents;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/style.css';
 import './css/nav.css';
 import './css/dashboard.css';
+
+import setupNavEvents from './components/navbar';
+
+setupNavEvents();
